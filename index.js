c34533783ec7549d2080ad7e7e8b310b6603aaeb
jullydang <38917143+jullydang@users.noreply.github.com>
2025-06-25 04:50:46 +0700

    index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const scrapeDexScreener = require('./scraper');
 
 (async () => {
   try {
-    const data = await scrapeDexScreener();
-    console.log('✅ Scraped Data:', JSON.stringify(data, null, 2));
+    await scrapeDexScreener();
+    console.log('✅ Scraping complete');
   } catch (err) {
     console.error('❌ Error scraping:', err);
   }
