8b8752351dd2b08208936090e554b8aaa44682ed
jullydang <38917143+jullydang@users.noreply.github.com>
2025-06-25 04:56:17 +0700

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const scrapeDexScreener = require('./scraper');
 (async () => {
   try {
     await scrapeDexScreener();
-    console.log('✅ Scraping complete');
   } catch (err) {
     console.error('❌ Error scraping:', err);
   }
